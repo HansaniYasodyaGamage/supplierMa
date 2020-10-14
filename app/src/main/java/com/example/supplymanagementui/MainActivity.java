c694ad0bb4ca07d383fff2294f50fef7c746a81a
Sandali Senarathna <sandalisenarathna@gmail.com>
2020-10-14 13:19:46 +0530

    login fullscreen

--- app/src/main/java/com/example/supplymanagementui/MainActivity.java
+++ app/src/main/java/com/example/supplymanagementui/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.supplymanagementui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardView = findViewById(R.id.addOrder);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
