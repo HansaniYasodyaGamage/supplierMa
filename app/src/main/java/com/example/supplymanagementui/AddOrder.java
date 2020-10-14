0f1287df8fc140df8d237944ee08671a68675c97
Sandali Senarathna <sandalisenarathna@gmail.com>
2020-10-14 21:38:49 +0530

    order interface validation done

--- app/src/main/java/com/example/supplymanagementui/AddOrder.java
+++ app/src/main/java/com/example/supplymanagementui/AddOrder.java
@@ -6,21 +6,64 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AddOrder extends AppCompatActivity {
 
     Button btnNext;
+
+    EditText edtTxtDeliveryAddress;
+    EditText edtTxtDeliveryDate;
+    EditText edtTxtRequisitioner;
+    EditText edtTxtMaterial;
+
+    String address;
+    String date;
+    String req;
+    String material;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_order);
 
+
+        edtTxtDeliveryAddress = findViewById(R.id.edtTxtDeliveryAddress);
+        edtTxtDeliveryDate = findViewById(R.id.edtTxtDeliveryDate);
+        edtTxtRequisitioner = findViewById(R.id.edtTxtRequisitioner);
+        edtTxtMaterial = findViewById(R.id.edtTxtMaterial);
+
+
+
+
+
         btnNext = findViewById(R.id.btnNext);
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(AddOrder.this, AddOrder2.class);
-                startActivity(intent);
+
+                address = edtTxtDeliveryAddress.getText().toString();
+                date = edtTxtDeliveryDate.getText().toString();
+                req = edtTxtRequisitioner.getText().toString();
+                material = edtTxtMaterial.getText().toString();
+
+                if(address.isEmpty()){
+                    edtTxtDeliveryAddress.setError("Please provide Address");
+                    edtTxtDeliveryAddress.requestFocus();
+                }else if(date.isEmpty()){
+                    edtTxtDeliveryDate.setError("Please provide Date");
+                    edtTxtDeliveryDate.requestFocus();
+                }else if(req.isEmpty()){
+                    edtTxtRequisitioner.setError("Please provide Requisitioner");
+                    edtTxtRequisitioner.requestFocus();
+                }else if(material.isEmpty()){
+                    edtTxtMaterial.setError("Please provide Material");
+                    edtTxtMaterial.requestFocus();
+                }else{
+                    Intent intent = new Intent(AddOrder.this, AddOrder2.class);
+                    startActivity(intent);
+                }
+
 
             }
         });
